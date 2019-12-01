cd391f65b9c08e1e3980dd2b309af4ae3c9287b7
Murat Ambarkutuk <m.ambarkutuk@gmail.com>
2019-12-01 09:43:56 -0500

    Change the design of the process...

--- FrameCapturing/CaptureFrame.py
+++ FrameCapturing/CaptureFrame.py
@@ -1,28 +1,48 @@
 import cv2
+import matplotlib.pyplot as plt
 
+class ARTracker:
+    def __init__(self, settings):
+        self.video = cv2.VideoCapture(settings["video"])
+        if self.video.isOpened() is False:
+            raise IOError("Video is not found!")
 
-class CaptureFrame:
-
-    def __init__(self, video):
-        self.video = video
-
-    def startCapturing(self):
-        while True:
-            check, frame = self.video.read()
-            cv2.imshow("Webcamera", frame)
-            key = cv2.waitKey(1)
-            if key == ord('s'):
-                break
-        pic = cv2.imwrite("Image.jpg", frame)
-        self.destructor()
-
-    def cameraDestructor(self):
+    def __del__(self):
         self.video.release()
         cv2.destroyAllWindows
+        plt.close()
+
+    def __call__(self):
+        '''Design the process here.'''
+        while True:
+            _, frame = self.readImage()
+            if ARTracker.showImage(frame) is not True:
+                break
+
+    def readImage(self):
+        check, frame = self.video.read()
+        return check, frame
+
+    @staticmethod
+    def showImage(img):
+        try:
+            cv2.imshow("Webcamera", img)
+            key = cv2.waitKey(1)
+            if key == ord('s'):
+                print("Quitting...")
+                return False
+        except KeyboardInterrupt as e:
+            print("Quitting...")
+        except Exception as e:
+            print(e)
+
+        return True
+
+    @staticmethod
+    def saveImage(path, img):
+        cv2.imwrite(path, img)
 
 
-video = cv2.VideoCapture(0)
-capture = CaptureFrame(video)
-capture.startCapturing()
-
+if __name__ == "__main__":
+    pass
 
